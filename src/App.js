f9a6b53751c400955311d1d3f7c97a0ddd6af52b
Arpit Verma <arpitv970@gmail.com>
2022-01-06 15:52:14 +0530

    Added Sponsors component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Destination from './components/Destination';
 import Navbar from './components/Navbar';
+import Sponsors from './components/Sponsors';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Destination />
+      <Sponsors />
     </div>
   );
 }
