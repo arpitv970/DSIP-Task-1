39a07c4ceef395dae4ba13f6efa7d0742c3fcf15
Arpit Verma <arpitv970@gmail.com>
2022-01-06 15:20:18 +0530

    Added Navbar with image slider besides

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,25 +3,47 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="https://www.gannett-cdn.com/presto/2019/06/24/USAT/d74b6f92-f007-40b9-b3f9-de33a7ac747a-AFP_AFP_1HH49E.JPG?width=2560" class="d-block w-100" alt="..." />
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">LOOP</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+
+            <div id="carouselExampleFade" className="my-carousel carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src="https://www.gannett-cdn.com/presto/2019/06/24/USAT/d74b6f92-f007-40b9-b3f9-de33a7ac747a-AFP_AFP_1HH49E.JPG?width=2560" className="d-block w-100" alt="..." />
                   </div>
-                  <div class="carousel-item">
-                    <img src="https://cooltravelingtips.com/wp-content/uploads/2020/03/skyscraper-3184798_1920-1024x683.jpg" class="d-block w-100" alt="..." />
+                  <div className="carousel-item">
+                    <img src="https://cooltravelingtips.com/wp-content/uploads/2020/03/skyscraper-3184798_1920-1024x683.jpg" className="d-block w-100" alt="..." />
                   </div>
-                  <div class="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1613419381378-b689dde48381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" class="d-block w-100" alt="..." />
+                  <div className="carousel-item">
+                    <img src="https://images.unsplash.com/photo-1613419381378-b689dde48381?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" className="d-block w-100" alt="..." />
                   </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="visually-hidden">Next</span>
                 </button>
             </div>
         </div>
