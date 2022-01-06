ff757d3224850f7124d03691a2133a0c4e646f7d
Arpit Verma <arpitv970@gmail.com>
2022-01-06 15:19:16 +0530

    Destination component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Destination from './components/Destination';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Destination />
     </div>
   );
 }
