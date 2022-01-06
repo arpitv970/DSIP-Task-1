f536d100eedba4fb350383bdade5517cc176c0bd
Arpit Verma <arpitv970@gmail.com>
2022-01-06 15:22:26 +0530

    Destination.js component created

---
+++ src/components/Destination.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Destination() {
+    return (
+        <div className="destinations">
+            <h1>Popular Destination</h1>
+            <div>Travel best spots around Globe</div>
+            <div className='locations'>
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1792&q=80' height='250px'/>
+                    <h3>Japan</h3>
+                    <p></p>
+                </div>            
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1491557345352-5929e343eb89?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' height='250px'/>
+                    <h3>Europe</h3>
+                    <p></p>
+                </div>            
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1518391846015-55a9cc003b25?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' height='250px'/>
+                    <h3>New York</h3>
+                    <p></p>
+                </div>   
+            </div>         
+            <div className='locations'>
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' height='250px'/>
+                    <h3>Dubai</h3>
+                    <p></p>
+                </div>            
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1585116938581-9d993839191c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80' height='250px'/>
+                    <h3>Varanasi</h3>
+                    <p></p>
+                </div>            
+                <div className='location-item'>
+                    <img src='https://images.unsplash.com/photo-1611043714658-af3e56bc5299?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' height='250px'/>
+                    <h3>Maldives</h3>
+                    <p></p>
+                </div>   
+            </div>         
+        </div>
+    )
+}
+
+export default Destination
