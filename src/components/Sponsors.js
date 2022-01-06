3b4ea687be3ef6b31f0682d5e03da918cf296569
Arpit Verma <arpitv970@gmail.com>
2022-01-06 15:54:25 +0530

    Created Sponsors component

---
+++ src/components/Sponsors.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Sponsors() {
+    return (
+        <div className='sponsors'>
+            <h1>Our Sponsors</h1>
+            <div className="sponsor-logos">
+                <div className="sponsor-item">
+                    <img src='https://lh3.googleusercontent.com/Ug17TTiDjCaPTygdf5vfP8NFlFbh0YXobK88injAlVJadzqkJhceDied9hifuT-4s-U' height='75px' width='75px' />
+            </div>
+                <div className="sponsor-item">
+                    <img src='https://lh3.googleusercontent.com/Ug17TTiDjCaPTygdf5vfP8NFlFbh0YXobK88injAlVJadzqkJhceDied9hifuT-4s-U' height='75px' width='75px' />
+                </div>
+                <div className="sponsor-item">
+                    <img src='https://lh3.googleusercontent.com/Ug17TTiDjCaPTygdf5vfP8NFlFbh0YXobK88injAlVJadzqkJhceDied9hifuT-4s-U' height='75px' width='75px' />
+                </div>
+                <div className="sponsor-item">
+                    <img src='https://lh3.googleusercontent.com/Ug17TTiDjCaPTygdf5vfP8NFlFbh0YXobK88injAlVJadzqkJhceDied9hifuT-4s-U' height='75px' width='75px' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sponsors
