608b728c379a9a9a915095c35f30b5247cda7460
Arpit Verma <arpitv970@gmail.com>
2022-01-06 16:29:11 +0530

    Created Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <h4>All copyright reserved | 2021 - 2022</h4>
+        </div>
+    )
+}
+
+export default Footer
